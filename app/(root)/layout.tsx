d085bcf85a6c3fdfbbc73d8de4d62609f0899dc8
nstanogias <nstanogias@gmail.com>
2025-10-04 18:16:05 +0300

    added tradingview widget

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,14 +1,12 @@
-import Header from "@/components/Header";
+import Header from '@/components/Header';
 
-const Layout = ({ children }: { children : React.ReactNode }) => {
-    return (
-        <main className="min-h-screen text-gray-400">
-            <Header />
+const Layout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <main className='min-h-screen text-gray-400'>
+      <Header />
 
-            <div className="container py-10">
-                {children}
-            </div>
-        </main>
-    )
-}
-export default Layout
+      <div className='container py-10'>{children}</div>
+    </main>
+  );
+};
+export default Layout;
